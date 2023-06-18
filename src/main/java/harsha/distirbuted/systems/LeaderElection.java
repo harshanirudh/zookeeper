696984230f9e2678c7ac09ac22496207a82e275d
harshaanirudh <harsha.anirudh@gmail.com>
2023-06-17 22:35:30 -0700

    Leader election

---
+++ src/main/java/harsha/distirbuted/systems/LeaderElection.java
@@ -0,0 +1,97 @@
+package harsha.distirbuted.systems;
+
+import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.Stat;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+
+public class LeaderElection implements Watcher {
+    private static final String ZOOKEEPER_ADDRESS = "localhost:2181";
+    private static final int SESSION_TIMEOUT = 3000;
+    private ZooKeeper zooKeeper;
+    private static final String ELECTION_NAMESPACE = "/election";
+    private String currentZnodeName;
+
+    public static void main(String[] args) throws IOException, InterruptedException, KeeperException {
+        LeaderElection election = new LeaderElection();
+        election.connectToZookeeper();
+        election.volunteerForLeadership();
+        election.reelectLeader();
+        election.run();
+        election.close();
+
+    }
+
+    private void run() throws InterruptedException {
+        synchronized (zooKeeper) {
+            zooKeeper.wait();
+        }
+    }
+
+    private void close() throws InterruptedException {
+        this.zooKeeper.close();
+    }
+
+    public void connectToZookeeper() throws IOException {
+        this.zooKeeper = new ZooKeeper(ZOOKEEPER_ADDRESS, SESSION_TIMEOUT, this);
+    }
+
+    public void volunteerForLeadership() throws InterruptedException, KeeperException {
+        String znodePrefix = ELECTION_NAMESPACE + "/c_";
+        String znodeFullPath = this.zooKeeper.create(znodePrefix, new byte[]{}, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+
+        System.out.println("Znode name:" + znodeFullPath);
+        this.currentZnodeName = znodeFullPath.replace(ELECTION_NAMESPACE + "/", "");
+
+    }
+
+    public void reelectLeader() throws InterruptedException, KeeperException {
+        Stat predecessorStat = null;
+        String predecessorZnodeName = "";
+        while (predecessorStat == null) {
+            List<String> children = zooKeeper.getChildren(ELECTION_NAMESPACE, false);
+            Collections.sort(children);
+            String smallestChild = children.get(0);
+
+            if (smallestChild.equals(currentZnodeName)) {
+                System.out.println("Iam the leader");
+                return;
+            } else {
+                System.out.println("Im not the leader");
+                int predecessorNodeIdx = Collections.binarySearch(children, currentZnodeName) - 1;
+                predecessorZnodeName = children.get(predecessorNodeIdx);
+                predecessorStat = zooKeeper.exists(ELECTION_NAMESPACE + "/" + predecessorZnodeName, this);
+
+            }
+        }
+
+        System.out.println("Watching znode " + predecessorZnodeName);
+        System.out.println();
+    }
+
+    @Override
+    public void process(WatchedEvent watchedEvent) {
+        switch (watchedEvent.getType()) {
+            case None:
+                if (watchedEvent.getState() == Event.KeeperState.SyncConnected) {
+                    System.out.println("Successfully connected to zookeeper");
+                } else {
+                    synchronized (zooKeeper) {
+                        System.out.println("Disconnect Event from zookeeper");
+                        zooKeeper.notifyAll();
+                    }
+                }
+                break;
+            case NodeDeleted:
+                try {
+                    reelectLeader();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                } catch (KeeperException e) {
+                    throw new RuntimeException(e);
+                }
+        }
+    }
+}
